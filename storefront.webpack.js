e9fa6f3829c1ee8f26407a2b3743c6d15bb143ed
Eduardo <edu.vlemes@gmail.com>
2024-04-10 22:36:53 -0300

    up

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,6 +4,7 @@ module.exports = () => ({
   resolve: {
     alias: {
       './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config'),
+      './base-config/sections.js': path.resolve(__dirname, 'template/js/netlify-cms/base-config/sections.js'),
     //   './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),   
     //   './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.js'),   
     //   './html/ProductGallery.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductGallery.html'),   
