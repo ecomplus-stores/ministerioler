e7f0e8994f4109e3630961597a69b70516ae34eb
Eduardo <edu.vlemes@gmail.com>
2024-04-23 23:10:29 -0300

    up

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,3 +1,7 @@
+const EcomPassport = require('@ecomplus/passport-client')
+
+const search = new EcomSearch()
+
 let mouseDown = false;
 let startX, scrollLeft;
 const sliders = document.querySelectorAll('.breakline-false');
@@ -57,5 +61,49 @@ $(document).ready(function(){
   });
 
   $('body').css('--header-vh-main', ($('header#header').innerHeight()) + 'px');
+
+  placeFavoritesAside();
 });
 
+$('body').on('click','#wishlist-button, .favorites__aside  .apx-side-heading button', function(){
+  $('#favorites-quickview').toggleClass('visible')
+});
+
+$('body').on('click','.product-card__favorite-remove', function(){
+  toggleFavoriteFront(this);
+});
+
+$('body').on('click','.product-card__favorite', function(){
+  placeFavoritesAside();
+})
+
+
+async function placeFavoritesAside(){
+  $(`.favorites__body`).empty();
+  try {
+    const { favorites } = await EcomPassport.ecomPassport.getCustomer();    
+    search.setProductIds(favorites).fetch().then(result => {
+      $.each(result.hits.hits, function(k,i){
+        let item = i._source;
+        $(`<div class="row item"><div class=col-12><div class="favorite-list product-card"data-sku=${item.sku} data-product-id=${item._id}><section><a class=product-card__link href=/${item.slug} title=${item.name}><div class=product-card__pictures><img alt="${item.pictures ? item.pictures[0].normal.alt : ''}"src="${item.pictures ? item.pictures[0].normal.url : '/assets/img-placeholder.png'}"></div></a><div class=product-card__content-group><div class='row align-items-start'><div class=col><a class=product-card__link href=/${item.slug} title=${item.name}><h3 class=product-card__name>${item.name}</h3></a></div><div class=col-auto><button class="btn product-card__favorite-remove"aria-label="Remover dos favoritos"><svg fill=none height=16 viewBox="0 0 16 16"width=16 xmlns=http://www.w3.org/2000/svg><g clip-path=url(#clip0_21_6728)><path d="M5.33331 8.00004H10.6666M14.6666 8.00004C14.6666 11.6819 11.6819 14.6667 7.99998 14.6667C4.31808 14.6667 1.33331 11.6819 1.33331 8.00004C1.33331 4.31814 4.31808 1.33337 7.99998 1.33337C11.6819 1.33337 14.6666 4.31814 14.6666 8.00004Z"stroke=#666666 stroke-linecap=round stroke-linejoin=round /></g><defs><clipPath id=clip0_21_6728><rect fill=white height=16 width=16 /></clipPath></defs></svg></button></div></div><div class="prices product-card__prices "><span class='prices__compare ${item.base_price ? '' : 'd-none'}'><s>${item.base_price ? (item.currency_symbol + ' ' +item.base_price.toLocaleString('pt-br', {style: 'decimal', maximumFractionDigits: 2,minimumFractionDigits: 2})) : ''}</s> </span><strong class=prices__value>${item.currency_symbol} ${item.price.toLocaleString('pt-br', {style: 'decimal', maximumFractionDigits: 2,minimumFractionDigits: 2})}</strong></div><div class="fade product-card__buy"><button class="btn btn-primary btn-sm"data-id=${item._id} type=button>Adicionar ao Carrinho</button></div></div></section><div></div></div></div>`).appendTo(`.favorites__body`);
+      });      
+    })
+    
+  }catch(e){
+    console.log(e)
+  }
+}
+
+async function toggleFavoriteFront(el){
+  try {
+    let productId = $(el).attr('data-id');
+    const { favorites } = await EcomPassport.ecomPassport.getCustomer();   
+    const favIndex = favorites.indexOf(productId)
+    favorites.splice(favIndex, 1) 
+    EcomPassport.ecomPassport.requestApi('/me.json', 'patch', { favorites })
+    $(el).closest('.item').remove();
+    
+  }catch(e){
+    console.log(e)
+  }
+}
