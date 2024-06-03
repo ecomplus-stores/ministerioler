f6d84d4adcdaf2a30615698090cbd661d75fe533
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2024-06-03 12:41:38 -0300

    Update check-group.js

--- template/js/check-group.js
+++ template/js/check-group.js
@@ -5,8 +5,6 @@ import { debounce } from 'perfect-debounce'
 export default (isCheckout = false) => {
   const checkGroup = debounce(() => {
     const customerDoc = ecomPassport.getCustomer().doc_number
-    /* window.checkedGroupDoc = customerDoc
-    window.sessionStorage.setItem('isGroup', 1) */
     if (customerDoc && customerDoc !== window.checkedGroupDoc) {
       window.axios.get(
         'https://us-central1-search-by-clients-group.cloudfunctions.net/app/check-group' +
@@ -15,7 +13,7 @@ export default (isCheckout = false) => {
         .then(({ data }) => {
           if (data.hasGroup) {
             window.checkedGroupDoc = customerDoc
-            window.sessionStorage.setItem('isGroup', 1)
+            window.sessionStorage.setItem('isGroup', data.value)
             window.sessionStorage.setItem('specSearch', JSON.stringify(data))
             window.EcomSearch.dslMiddlewares.push((dsl) => {
               dsl.query.bool.filter.push({
