9760c674b6fb5e67cc8218de9b7ef1f125114649
matheusgnreis <matheus@e-com.club>
2024-07-11 07:24:57 -0300

    chore: update checkout shipping calculator

--- template/js/custom-js/html/ShippingCalculator.js
+++ template/js/custom-js/html/ShippingCalculator.js
@@ -273,15 +273,15 @@ import {
   
       setSelectedService (i) {
         if (this.canSelectServices) {
-          this.$emit('select-service', this.shippingServices[i])
+          this.$emit('select-service', this.shippingServicesFinal[i])
           this.selectedService = i
           const eventLayer = window.dataLayer.find(({event}) => event === 'eec.checkout')
           window.dataLayer.push({
             event: 'add_shipping_info',
             ecommerce: {
               currency: 'BRL',
-              shipping_method: this.shippingServices[i] && this.shippingServices[i].label,
-              value: this.shippingServices[i] && this.shippingServices[i].shipping_line && this.shippingServices[i].shipping_line.total_price,
+              shipping_method: this.shippingServicesFinal[i] && this.shippingServicesFinal[i].label,
+              value: this.shippingServicesFinal[i] && this.shippingServicesFinal[i].shipping_line && this.shippingServicesFinal[i].shipping_line.total_price,
               items: eventLayer && eventLayer.ecommerce && eventLayer.ecommerce.checkout && eventLayer.ecommerce.checkout.products
             }
           })
